5c0e021ace1e37154cbe5c039cfe96bd01f5d72a
tungdibui2609 <tungdibui2609@gmail.com>
2026-05-01 16:21:49 +0700

    bbbbbbbbbbbbbbbbbbbbbb

--- k_sql/add_product_color.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE public.products ADD COLUMN color varchar(50);
--- tmp/add_perm.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO permissions (code, name, module, description)
-VALUES 
-('lot.create', 'Tạo LOT Sản xuất', 'Hàng hóa', 'Cho phép tạo mới các lô hàng sản xuất'),
-('warehouse_lot.create', 'Tạo Quản lý LOT', 'Hàng hóa', 'Cho phép tạo mới các lô hàng trong quản lý LOT')
-ON CONFLICT (code) DO NOTHING;
